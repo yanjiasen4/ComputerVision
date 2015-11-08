b18f6eeb062e1c71419d43df748c46ae55205db3
yanjiasen4 <1158076176@qq.com>
2015-11-08 14:25:42 +0800

    作业更新到高级形态学运算

---
+++ 二值图像处理（额外）/5130379022_homework1_extra.py
@@ -0,0 +1,111 @@
+import cv2 as cv2 
+import numpy as np
+import math
+import sys
+
+def getAllPiexl(hist):
+    total = 0
+    for i in range(0,256):
+        total += hist[i][0]
+    return total
+
+def getFirstLast(hist):
+    for i in range(0,256):
+        if hist[i][0] != 0:
+            first = i
+            break
+    for j in range(255,first,-1):
+        if hist[j][0] != 0:
+            last = j
+            break
+    return first,last
+
+def OTSUthreshold(hist):
+    T = 0
+    maxV = 0
+    v = 0
+    total = getAllPiexl(hist)
+    first,last = getFirstLast(hist)
+    for i in range(first,last):
+        if hist[i][0] == 0: continue
+        t1 = 0
+        t2 = 0
+        s1 = 0
+        s2 = 0
+        for j in range(first,i+1):
+            t1 += hist[j][0]*j
+            s1 += hist[j][0]
+        for k in range(i+1,last+1):
+            t2 += hist[k][0]*k
+            s2 += hist[k][0]    
+        t1 = t1/s1
+        t2 = t2/s2
+        w1 = s1/total
+        w2 = 1-w1
+        v = w1*w2*(t1-t2)*(t1-t2)
+        if maxV < v:
+            maxV = v
+            T = i
+    print "阈值为",T
+    return T
+
+def Fuzzythreshold(hist):
+    first,last = getFirstLast(hist)
+    T = -1
+    bestV = sys.maxint
+    v = 0
+    mu = 0
+    S = []
+    W = []
+    for i in range(0,first):
+        S.append(-1.0)
+        W.append(-1.0)
+    S.append(hist[first][0])
+    W.append(0)
+    total = getAllPiexl(hist)
+    for i in range(first,last+1):
+       S.append(S[i] + hist[i][0])
+       W.append(W[i] + hist[i][0]*i)
+    
+    Smu = []
+    Smu.append(0)
+    #记录查找表，加快计算速度
+    for i in range(1,last+1):
+        mu = 1/(1.0+(i+0.0)/(last-first))
+        Smu.append(-mu*math.log(mu)-(1-mu)*math.log(1-mu))
+
+    for i in range(first,last):
+        v = 0
+        mu = W[i]/S[i]
+        for j in range(first,i+1):
+            v += Smu[int(abs(j-mu))]*hist[j][0]
+        if S[last]-S[i] == 0: continue 
+        mu = (W[last]-W[i])/(S[last]-S[i])
+        for k in range(i+1,last+1):
+            v += Smu[int(abs(k-mu))]*hist[k][0]
+        if bestV > v:
+            bestV = v
+            T = i-1
+    print "阈值为",T
+    return T
+
+#fileimg = cv2.imread("test.jpg")
+fileimg = cv2.imread("testfromnet.png") # 读取图片
+img = cv2.cvtColor(fileimg,cv2.COLOR_BGR2GRAY)
+hist = cv2.calcHist([img],[0],None,[256],[0.0,255.0])
+print "请选择阈值选取方式"
+print "1. Otsu算法"
+print "2. 模糊集算法"
+select = int(input())
+if select == 1:
+    T = OTSUthreshold(hist)
+elif select == 2:
+    T = Fuzzythreshold(hist)
+else:
+    print "error!"
+    exit()
+retval, newimg=cv2.threshold(img,T,255,cv2.THRESH_BINARY)      
+cv2.imshow('threshold',newimg)
+print "按任意键终止程序"
+cv2.waitKey(0)    
+cv2.destroyAllWindows() 
---
+++ 高级形态学运算（额外）/thinning.py
@@ -0,0 +1,146 @@
+#coding=utf-8
+import cv2
+import numpy as np
+import copy
+import imglab # a thinning algorithm from internet
+
+ele1 = [[255,255,255],[-1,0,-1],[0,0,0]]
+ele2 = [[-1,255,255],[0,0,255],[0,0,-1]]
+ele3 = [[0,-1,255],[0,0,255],[0,-1,255]]
+ele4 = [[0,0,-1],[0,0,255],[-1,255,255]]
+ele5 = [[0,0,0],[-1,0,-1],[255,255,255]]
+ele6 = [[-1,0,0],[255,0,0],[255,255,-1]]
+ele7 = [[255,-1,0],[255,0,0],[255,-1,0]]
+ele8 = [[255,255,-1],[255,0,0],[-1,0,0]]
+
+# return height,length of img
+def getImgSize(img):
+    return len(img),len(img[0])
+
+def IsA(array,i,j):
+    tmp = [array[i][j],array[i-1][j],array[i-1][j+1],array[i][j+1],array[i+1][j+1],array[i+1][j],array[i+1][j-1],array[i][j-1],array[i-1,j-1]]
+    n=0
+    for i in range(1,8):
+        if tmp[i]==0 and tmp[i+1]==255:
+            n = n+1
+    return n
+
+# Zhang-thinning algorithmn
+def Zhangthin(img):
+    mark=1
+    height = len(img)
+    length = len(img[0])
+    while mark==1:
+        mark=0
+        for i in range(1,height-1):
+            for j in range(1,length-1):
+                cond = 0
+                if img[i][j]==255:
+                    n = 0
+                    for ii in range(-1,2):
+                        for jj in range(-1,2):
+                            n = n+(img[i+ii][j+jj])/255
+                    if n >=3 and n <= 7:
+                        cond += 1
+                    if IsA(img,i,j)==1:
+                        cond += 1
+                    if (int(img[i-1][j])*img[i][j+1]*img[i+1][j])==0:
+                        cond += 1
+                    if (int(img[i][j+1])*img[i+1][j]*img[i][j-1])==0:
+                        cond += 1
+                    if cond == 4:
+                        mark = 1
+                        img[i][j] = 0
+
+        for i in range(1,height-1):
+            for j in range(1,length-1):
+                cond = 0
+                if img[i][j]==255:
+                    n = 0
+                    for ii in range(-1,2):
+                        for jj in range(-1,2):
+                            n = n + img[i+ii][j+jj]
+                    if n>=3 and n<=7:
+                        cond += 1
+                    if IsA(img,i,j)==1:
+                        cond += 1
+                    if (int(img[i-1][j])*img[i][j+1]*img[i][j-1])==0:
+                        cond += 1
+                    if (int(img[i][j-1])*img[i+1][j]*img[i][j-1])==0:
+                        cond += 1
+                    if cond == 4:
+                        mark = 1
+                        img[i][j] = 0
+
+        return img
+
+# fill the img's edge with white color
+def edgeFilling(img):
+    height = len(img)
+    length = len(img[0])
+    for i in range(0,length):
+        img[0][i] = 255
+        img[height-1][i] = 255
+    for j in range(0,height):
+        img[j][0] = 255
+        img[j][length-1] = 255
+    return img
+
+# hit-or-miss operation                  
+def homTransform(img,ele):
+    height = len(img)
+    length = len(img[0])
+    h_off = len(ele)/2
+    l_off = len(ele[0])/2
+    source = copy.deepcopy(img)
+    for i in range(h_off,height-h_off):
+        for j in range(l_off,length-l_off):
+            flag = 0
+            for ii in range(-h_off,h_off+1):
+                if flag == 1: break
+                for jj in range(-l_off,l_off+1):
+                    imgbit = source[i+ii][j+jj]
+                    img[i][j] = 0
+                    eletmp = ele[h_off+ii][l_off+jj]
+                    if eletmp == -1: continue
+                    elif eletmp != imgbit:
+                        flag = 1
+                        img[i][j] = 255
+                        break
+    return img
+
+#src and dest must be the same size
+def diffImg(src,tmp):
+    height,length = getImgSize(src)
+    for i in range(0,height):
+        for j in range(0,length):
+            if src[i][j] == 0:
+                if tmp[i][j] == 0:
+                    src[i][j] = 255
+    return src
+
+# thinning by a single structuring element
+def ThinningByEle(img,ele):
+    source = copy.deepcopy(img)
+    img = homTransform(img,ele)
+    return diffImg(source,img)
+
+# thinning by a list of structuring elements
+def Thinning(img,eleList):
+    t = len(eleList)
+    for i in range(0,t):
+        img = ThinningByEle(img,eleList[i])
+        img = edgeFilling(img)
+    return img
+                               
+fileimg = cv2.imread('potota.jpg')
+img = cv2.cvtColor(fileimg,cv2.COLOR_BGR2GRAY)
+retval, img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+img = cv2.bitwise_not(img)
+eleList = [ele1,ele2,ele3,ele4,ele5,ele6,ele7,ele8]
+res = Thinning(img,eleList)
+for i in range(20):
+    res = Thinning(res,eleList)
+cv2.imshow("result",res)
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
