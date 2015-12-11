bb29581eed45d275d6c1d80459b55e9540005e4e
yanjiasen4 <1158076176@qq.com>
2015-12-11 21:24:31 +0800

    SIFT特征提取
    自己实现drawMatches

--- SIFT特征提取/SIFT.py
+++ SIFT特征提取/SIFT.py
@@ -2,12 +2,83 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('test.png')
-gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+def drawMatches(img1, kp1, img2, kp2, matches):
+    """
+    I couldn't find drawMatches() in OpenCV
+    My own implementation of cv2.drawMatches as OpenCV 2.4.9
+    does not have this function available but it's supported in
+    OpenCV 3.0.0
+    """
+    # Create a new output image that concatenates the two images together
+    # (a.k.a) a montage
+    rows1 = img1.shape[0]
+    cols1 = img1.shape[1]
+    rows2 = img2.shape[0]
+    cols2 = img2.shape[1]
+
+    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+
+    # Place the first image to the left
+    out[:rows1,:cols1] = np.dstack([img1, img1, img1])
+
+    # Place the next image to the right of it
+    out[:rows2,cols1:] = np.dstack([img2, img2, img2])
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for mat in matches:
+
+        # Get the matching keypoints for each of the images
+        img1_idx = mat[0].queryIdx
+        img2_idx = mat[0].trainIdx
+
+        # x - columns
+        # y - rows
+        (x1,y1) = kp1[img1_idx].pt
+        (x2,y2) = kp2[img2_idx].pt
+
+        # Draw a small circle at both co-ordinates
+        # radius 4
+        # colour blue
+        # thickness = 1
+        cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)
+        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+
+        # Draw a line in between the two points
+        # thickness = 1
+        # colour blue
+        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
+        
+    # Show the image
+    cv2.imshow('Matched Features', out)
+    cv2.waitKey(0)
+    cv2.destroyWindow('Matched Features')
+    return out
+
+img1 = cv2.imread('test1.png')
+img2 = cv2.imread('test2.png')
+gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
 
 sift = cv2.SIFT()
-kp = sift.detect(gray,None)
 
-img=cv2.drawKeypoints(gray,kp)
+kp1, des1 = sift.detectAndCompute(gray1,None)
+kp2, des2 = sift.detectAndCompute(gray2,None)
 
-cv2.imwrite('sift_keypoints.jpg',img)
+# BFMatcher with default params
+bf = cv2.BFMatcher()
+
+matches = bf.knnMatch(des1,des2, k=2)
+
+# Apply ratio test
+good = []
+for m,n in matches:
+    if m.distance < 0.8*n.distance:
+        good.append([m])
+
+# cv2.drawMatchesKnn expects list of lists as matches.
+img3 = drawMatches(gray1,kp1,gray2,kp2,good)
+
+#plt.imshow(img3),plt.show()
+
+cv2.imwrite('sift_matches.jpg',img3)
