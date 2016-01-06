215dc9db79b10be3631ca4a45948c5e847f4f756
yanjiasen4 <1158076176@qq.com>
2016-01-06 15:46:01 +0800

    try to use gabor filter and block learning
    but it seems doesn't work

--- 脑肿瘤识别分类（大作业）/brain.py
+++ 脑肿瘤识别分类（大作业）/brain.py
@@ -8,7 +8,10 @@ import numpy as np
 N = 66
 data_width = 256
 data_length = 256
+block_width = 8
+block_length = 8
 data_size = data_width*data_length
+block_size = block_width*block_length
 
 dataRootPath = 'hg0005'
 dataPathName_FLAIR = dataRootPath+'\BRATS_HG0005_FLAIR'
@@ -23,10 +26,22 @@ def read_data(fileDir,filePre):
     for i in range(N):
         fileIndex = str(i + 67) + '';
         fileFullName = fileDir+filePre+fileIndex+'.png'
-        print fileFullName
+        print 'read file: ' + fileFullName
         tmp = cv2.imread(fileFullName,0)
         data[i] = tmp
     return data
+
+def mat2str(mat):
+    h,l = mat.shape[:2]
+    res = '[\n'
+    for i in range(h):
+        for j in range(l):
+            res += (str(mat[i][j]) + ',')
+        res += '\n'
+    for j in range(l):
+        res += '  '
+    res += ']\n'
+    return res
 
 def getImage(a,w,l):
     output = np.zeros((w,l),dtype=np.uint8)
@@ -35,17 +50,82 @@ def getImage(a,w,l):
             output[i][j] = a[i*w+j]
     return output
 
-test1 = read_data(dataPathName_FLAIR,testFileName_FLAIR)
-target = read_data(dataPathName_truth,testFileName_truth)
-net = buildNetwork(data_size, 10, data_size)
-ds = SupervisedDataSet(data_size, data_size)
-for i in range(N):
-    ds.addSample(np.ravel(test1[i]),np.ravel(target[i]))
-trainer = BackpropTrainer(net, ds)
-for i in range(10):
-    trainer.trainEpochs(1)
+# img: 256x256
+def trainByImg(net, test, target):
+    h,l = test.shape[:2]
+    for i in range(h):
+        for j in range(l):
+            if target[i,j] != 0:
+                target[i,j] = 1
 
-testImg = getImage(net.activate(np.ravel(test1[50])),data_length,data_width)
-cv2.imshow("test",testImg)
-cv2.waitKey(0)
-cv2.destroyAllWindows();
+    ds = SupervisedDataSet(block_size, block_size)
+    for i in range(0,data_length,block_width):
+        for j in range(0,data_width,block_length):
+            ds.addSample(np.ravel(test[i:i+block_length,j:j+block_width]), np.ravel(target[i:i+block_length,j:j+block_width]))
+
+    trainer = BackpropTrainer(net, ds)
+    for i in range(1):
+        trainer.trainEpochs(1)
+
+def activateByImg(net, inputTest):
+    output = np.zeros((data_length,data_width), dtype=np.int)
+    fileObj = open('res.txt','a')
+    for i in range(0,data_width,block_width):
+        for j in range(0,data_length,block_length):
+            output[i:i+block_length,j:j+block_width] = (net.activate(np.ravel(inputTest[i:i+block_length,j:j+block_width]))).reshape(block_length,block_width)
+            buffer_str = mat2str(output[i:i+block_length,j:j+block_width])
+            fileObj.write(buffer_str)
+    fileObj.close()
+    h,l = output.shape[:2]
+    for i in range(h):
+        for j in range(l):
+            if output[i][j] != 0:
+                print '!'
+                output[i][j] = 255
+    return output
+
+def usage():
+    print 'input a integer in [0,65] to get result of a input'
+    print 'input q to exit'
+
+
+# main
+def __main__():
+    tests = read_data(dataPathName_FLAIR,testFileName_FLAIR)
+    targets = read_data(dataPathName_truth,testFileName_truth)
+    '''
+    net = buildNetwork(data_size, 10, data_size)
+    ds = SupervisedDataSet(data_size, data_size)
+    for i in range(N):
+        ds.addSample(np.ravel(test1[i]),np.ravel(target[i]))
+    trainer = BackpropTrainer(net, ds)
+    for i in range(10):
+        trainer.trainEpochs(1)
+    testImg = getImage(net.activate(np.ravel(test1[50])),data_length,data_width)
+    '''
+    net = buildNetwork(block_size, 16, block_size)
+    for i in range(N):
+        print 'train: '+str(i)
+        trainByImg(net, tests[i], targets[i])
+
+    usage()
+    flag = True
+    while flag:
+        ipt = raw_input('>>')
+        if ipt.isalpha():
+            if ipt=='q':
+                print 'quit'
+                flag = False
+            else:
+                print 'error input: [%s]',ipt
+        elif ipt.isdigit():
+            num = int(ipt)
+            if num >= 0 and num <= 65:
+                res = activateByImg(net, tests[num])
+                cv2.imshow("test",res)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows();
+            else:
+                print('error index of tests [%d], should in [0,65]',num)
+
+__main__()
---
+++ 脑肿瘤识别分类（大作业）/gabor.py
@@ -0,0 +1,53 @@
+#-*- coding:gbk -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import ndimage as ndi
+
+from skimage import data
+from skimage import io
+from skimage.util import img_as_float
+from skimage import img_as_ubyte
+from skimage.filters import gabor_kernel
+from skimage.filters import gabor_filter
+from skimage.exposure import rescale_intensity
+import os
+dir_path = 'hg0005'
+flair_path = 'BRATS_HG0005_FLAIR'
+flair_res_path = 'BRATS_HG0005_FLAIR_GABOR'
+t1_path = 'BRATS_HG0005_T1'
+t1_res_path = 'BRATS_HG0005_T1_GABOR'
+t1c_path = 'BRATS_HG0005_T1C'
+testfile = 'BRATS_HG0005_FLAIR_77.png'
+
+# Plot a selection of the filter bank kernels and their responses.
+results = []
+
+freq = input("����gabor��frequency:")
+theta = input("����gabor��theta(float:�Ƕ�):")
+bandwidth = input("����gabor��bandwidth(float:Ĭ��1):")
+sigma_x = input("����gabor��sigma_x(float:Ĭ��None):")
+sigma_y = input("����gabor��sigma_y(float:Ĭ��None):")
+n_stds = input("����gabor��n_stds(scalar:Ĭ��3):")
+offset = input("����gabor��offset(float:Ĭ��0):")
+mode = raw_input("����gabor��mode(��constant��, ��nearest��, ��reflect��, ��mirror��, ��wrap��:Ĭ��'reflect'):")
+cval = input("����gabor��cval(scalar:Ĭ��0)")
+is_test = raw_input("�Ƿ����Ե���ͼƬ(Y/N):")
+# Plot a selection of the filter bank kernels and their responses.
+
+results = []
+if not os.path.exists(os.path.join(dir_path,flair_res_path)):
+    os.mkdir(os.path.join(dir_path,flair_res_path))
+if cmp(is_test,'Y') == 0:
+    tstimg = img_as_float(io.imread(os.path.join(dir_path,flair_path,testfile)))
+    reresult, result = gabor_filter(tstimg,frequency=freq,theta=theta/180.0*np.pi)
+    res_fname = "%s_test_%s" %(testfile[:testfile.rfind('.')],testfile[testfile.rfind('.'):])
+    io.imsave(os.path.join(dir_path,flair_res_path,res_fname),rescale_intensity(reresult,out_range=(-1.,1.)))
+else:
+    for str_root, lst_dir, lst_file in os.walk(os.path.join(dir_path,flair_path)):
+        for img_name in lst_file:
+            img = img_as_float(io.imread(os.path.join(dir_path,flair_path,img_name)))
+            #gabor �˲���
+            reresult, result = gabor_filter(img,frequency=freq,theta=theta/180.0*np.pi)
+            res_fname = "%s%s" %(img_name[:img_name.rfind('.')],img_name[img_name.rfind('.'):])
+            io.imsave(os.path.join(dir_path,flair_res_path,res_fname),rescale_intensity(reresult,out_range=(-1.,1.)))
